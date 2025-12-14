fe8a3e661c4c985aa0f379549f52b45ee3a23c1a
Vedant Sinha <sinhavedant0705@gmail.com>
2025-12-14 21:37:46 +0530

    feat: add toggle links for login and sign up modes

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -74,6 +74,31 @@ const LoginPage = () => {
           <input type="checkbox" />
           <p>Agree to the terms of use and privacy policy.</p>
         </div>
+
+        <div className="flex flex-col gap-2">
+          {currState === "Sign up" ? (
+            <p className="text-sm text-gray-600">
+              Already have an account? 
+              <span
+                onClick={() => {
+                  setCurrState("Login");
+                  setIsDataSubmitted(false)
+                }}
+                className="font-medium text-violet-500 cursor-pointer">
+                Login here
+              </span>
+            </p>
+          ) : (
+            <p className="text-sm text-gray-600">
+              Create an account 
+              <span
+                onClick={() => setCurrState("Sign up")}
+                className="font-medium text-violet-500 cursor-pointer">
+                Click here
+              </span>
+            </p>
+          )}
+        </div>
       </form>
     </div>
   );
