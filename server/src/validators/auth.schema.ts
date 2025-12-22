0bd8eb16ae25622b21a532d070fe9e3cd984037f
Vedant Sinha <sinhavedant0705@gmail.com>
2025-12-23 03:22:40 +0530

    feat: add Zod validation schema for authentication

---
+++ server/src/validators/auth.schema.ts
@@ -0,0 +1,16 @@
+import z, { email } from "zod";
+
+export const signupSchema = z.object({
+  fullName: z
+    .string()
+    .min(2, "Full name is too short")
+    .max(20, "Name cannot be more than 20 characters"),
+  email: z.string().email("Invalid Email Address"),
+  password: z
+    .string()
+    .min(6, "Password must be at least 6 characters")
+    .max(20, "Password cannot be more than 20 characters"),
+  bio: z.string(),
+});
+
+export type SignupInput = z.infer<typeof signupSchema>;
