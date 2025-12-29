afa7f4170d6e2d4fefef13f789c9c6a9639897ec
Vedant Sinha <sinhavedant0705@gmail.com>
2025-12-29 20:41:37 +0530

    chore: minor changes

--- server/src/types/IMessage.ts
+++ server/src/types/IMessage.ts
@@ -1,11 +1,11 @@
-import { Document, Types } from "mongoose";
+import { Types } from "mongoose";
 
-export interface IMessage extends Document {
+export interface IMessage {
   senderId: Types.ObjectId;
   receiverId: Types.ObjectId;
   text?: string;
   image?: string;
   seen: boolean;
-  createdAt: Date;
-  updatedAt: Date;
+  createdAt?: Date;
+  updatedAt?: Date;
 }
