0b87c90aadcd62f9e20b037adb2f6726370a5eab
Vedant Sinha <sinhavedant0705@gmail.com>
2025-12-31 02:47:38 +0530

    integrate send message controller with api route

--- server/src/routes/messageRoutes.ts
+++ server/src/routes/messageRoutes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.js";
-import { getMessages, getUsersForSidebar, markMessageAsSeen } from "../controllers/messageController.js";
+import { getMessages, getUsersForSidebar, markMessageAsSeen, sendMessage } from "../controllers/messageController.js";
 
 const messageRouter = express.Router();
 
@@ -12,5 +12,8 @@ messageRouter.get("/:id", protectRoute, getMessages);
 
 // Mark message as seen
 messageRouter.put("/mark/:id", protectRoute, markMessageAsSeen);
+
+// Send message to other user
+messageRouter.post("/send/:id", protectRoute, sendMessage)
 
 export default messageRouter;
