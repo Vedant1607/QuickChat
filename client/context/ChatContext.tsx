610653e44bb4fdec6d2feedda38be643415c4841
Vedant Sinha <sinhavedant0705@gmail.com>
2026-01-04 22:58:30 +0530

    feat(chat): add function to subscribe to messages for selected user

--- client/context/ChatContext.tsx
+++ client/context/ChatContext.tsx
@@ -52,6 +52,22 @@ export const ChatProvider = ({ children }) => {
     }
   };
 
+  // Function to subscribe to messages for selected user
+  const subscribeToMessages = async () => {
+    if (!socket) return;
+    socket.on("newMessage", (newMessage) => {
+      if(selectedUser && newMessage.senderId === selectedUser._id) {
+        newMessage.seen = true;
+        setMessages((prevMessages) => [...prevMessages, newMessage]);
+        const { data } = axios.put(`/api/messages/mark/${newMessage._id}`);
+      } else {
+        setUnseenMessages((prevUnseenMessages) => ({
+          ...prevUnseenMessages, [newMessage.senderId] : prevUnseenMessages[newMessage.senderId] ? prevUnseenMessages[newMessage.senderId] + 1 : 1
+        }))
+      }
+    })
+  }
+
   const value = {};
 
   return <ChatContext.Provider value={value}>{children}</ChatContext.Provider>;
