dc848d933423d943a86d0075071b7b58fa6723aa
Vedant Sinha <sinhavedant0705@gmail.com>
2026-01-10 21:25:21 +0530

    refactor(home): simplify layout and context usage

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,18 +1,26 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import ChatContainer from "../components/ChatContainer";
 import RightSidebar from "../components/RightSidebar";
 import Sidebar from "../components/Sidebar";
 import { ChatContext } from "../../context/ChatContext";
 
 const HomePage = () => {
+  const chatContext = useContext(ChatContext);
 
-  const { selectedUser } = useContext(ChatContext);
+  if (!chatContext) {
+    throw new Error("Homepage must be used within a ChatProvider");
+  }
+  const { selectedUser } = chatContext;
+
+  const gridLayout = selectedUser
+    ? "md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]"
+    : "md:grid-cols-2";
 
   return (
-    <div className="border w-full h-screen sm:px-[15%] sm:py-[5%]">
-      <div className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-[100%] grid grid-cols-1 relative ${selectedUser ? 
-        'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2'}
-      `}>
+    <div className="w-full h-screen sm:px-[15%] sm:py-[5%]">
+      <div
+        className={`relative h-full grid grid-cols-1 overflow-hidden rounded-2xl border-2 border-gray-600 backdrop-blur-xl ${gridLayout}`}
+      >
         <Sidebar />
         <ChatContainer />
         <RightSidebar />
